436f9c380f45b19949762c1a79e5750058ea054f
hector67100 <46929925+hector67100@users.noreply.github.com>
2025-01-24 14:47:07 -0400

    Update logout.php

--- web/logout.php
+++ web/logout.php
@@ -2,8 +2,9 @@
 /* 
  * Cierra la sesi贸n como usuario validado
  */
-
+session_start(); //iniciamos la sesion
 include('php_lib/login.lib.php'); //incluimos las funciones
+$_SESSION["profesional"] = null; //borramos la variable de sesion
 logout(); //vacia la session del usuario actual
 header('Location: login.php'); //saltamos a login.php
 
